f5e14d9ad18fa0b7d7ea19e4f91e4d6d6b3e7df2
Lena3266 <lenanyambane@gmail.com>
2025-08-25 12:51:51 +0300

    input.txt

---
+++ FileStructure.py
@@ -0,0 +1,10 @@
+# #print("File Structure") 
+# x = 123
+# y = 456
+# z = 657
+# def add(x, y, z):
+#     return x + y + z
+# print(add(x, y, z))
+file = open("readme.md", "r") 
+data = file.read()
+print(data)
